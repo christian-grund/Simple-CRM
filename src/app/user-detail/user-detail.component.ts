cdc8c4c0e74eaaa62eaf746af41c1b034f328e70
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-05-29 14:23:37 +0200

    26 - Nutzer mit ID aus der Firebase DB abrufen

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { ActivatedRoute } from "@angular/router";
+import { FirebaseService } from "../firebase-services/firebase.service";
+import { collection, doc, onSnapshot } from "@angular/fire/firestore";
+import { User } from "../../models/user.class";
 
 @Component({
-  selector: 'app-user-detail',
+  selector: "app-user-detail",
   standalone: true,
   imports: [MatCardModule],
-  templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss',
+  templateUrl: "./user-detail.component.html",
+  styleUrl: "./user-detail.component.scss"
 })
-export class UserDetailComponent implements OnInit {
+export class UserDetailComponent implements OnInit, OnDestroy {
   userId!: string;
+  user: User = new User();
+  unsubUser!: () => void;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private firebaseService: FirebaseService) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.userId = id !== null ? id : 'DefaultBankName';
-    console.log('userId:', this.userId);
+    const id = this.route.snapshot.paramMap.get("id");
+    this.userId = id !== null ? id : "DefaultBankName";
+    // console.log('userId:', this.userId);
+    this.unsubUser = this.getUser();
+  }
+
+  getUser() {
+    const userDocRef = doc(this.firebaseService.firestore, "users", this.userId);
+    return onSnapshot(userDocRef, user => {
+      if (user.exists()) {
+        this.user = new User(user.data());
+        console.log("user:", this.user);
+      } else {
+        console.log("No such document!");
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.unsubUser) {
+      this.unsubUser();
+    }
   }
 }
