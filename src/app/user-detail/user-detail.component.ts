86f632937c80cf2c6e9dc9a6e66e46a4b2eb02d6
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-05-29 11:40:25 +0200

    25 - So bekommst du die ID aus der URL

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,4 +9,14 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss',
 })
-export class UserDetailComponent {}
+export class UserDetailComponent implements OnInit {
+  userId!: string;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.userId = id !== null ? id : 'DefaultBankName';
+    console.log('userId:', this.userId);
+  }
+}
