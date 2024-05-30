35a91d40589a8ee78453e7ff44f6cac0d3282a34
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-05-30 09:51:22 +0200

    31 - Nutzer bearbeiten Component

--- src/app/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -9,16 +9,20 @@ import { FormsModule } from "@angular/forms";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { CommonModule } from "@angular/common";
 import { FirebaseService } from "../firebase-services/firebase.service";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { provideNativeDateAdapter } from "@angular/material/core";
 
 @Component({
   selector: "app-dialog-edit-user",
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule, FormsModule, MatProgressBarModule, CommonModule],
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule, FormsModule, MatProgressBarModule, MatDatepickerModule, CommonModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: "./dialog-edit-user.component.html",
   styleUrl: "./dialog-edit-user.component.scss"
 })
 export class DialogEditUserComponent {
   user!: User;
+  birthDate!: Date;
   loading: boolean = false;
 
   constructor(private firebase: FirebaseService, public dialogRef: MatDialogRef<DialogEditUserComponent>) {}
